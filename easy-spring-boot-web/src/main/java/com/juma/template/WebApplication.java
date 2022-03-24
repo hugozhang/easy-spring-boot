5be609cc8c5ff8ebbccf997c470d1c2822d9c0fa
hugozxh <381129269@qq.com>
2022-03-24 23:00:10 +0800

    这个工程是学习用的  相关的第三方工具测试

--- easy-spring-boot-web/src/main/java/com/juma/template/WebApplication.java
+++ easy-spring-boot-web/src/main/java/com/juma/template/WebApplication.java
@@ -1,9 +1,17 @@
 package com.juma.template;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.TypeExcludeFilter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
 
 @SpringBootApplication
+@ComponentScan(nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class)
 public class WebApplication {
 
     public static void main(String[] args) {
